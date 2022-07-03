1e0f85f607854488fbb06fb3cad4d2410b561dca
Aliza02 <alizaanxari08@gmail.com>
2022-07-03 14:39:12 +0500

    home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'quizeapp';
+
+  
+  constructor(){}
+
+  ngOnInit(): void {
+    }
+
+    submit(){
+      console.log('asda');
+          let containerFluid=document.querySelector('.container-fluid');
+        console.log(containerFluid);
+    }
+
 }
