3a0fd3a553f5175191837539e8bea6683e0cf083
Aliza02 <alizaanxari08@gmail.com>
2022-07-13 18:36:48 +0500

    questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,65 @@ export class AppComponent {
     }
 
     start(){
-      console.log('asda');
-          let containerFluid=document.querySelector('.container-fluid');
           let home=document.querySelector('.home');
           let question_1=document.querySelector('.question1');
-          let submit=document.querySelector('.start-btn');
             question_1?.classList.add('active');
-            home?.classList.remove('active');         
+            home?.classList.remove('active');      
+    }
+    
+    question2(){
+      let question_2=document.querySelector('.question2');
+      let question_1=document.querySelector('.question1');
+      question_1?.classList.remove('active');
+      question_2?.classList.add('active');
+    }
+    question3(){
+      let question_2=document.querySelector('.question2');
+      let question_3=document.querySelector('.question3');
+      question_2?.classList.remove('active');
+      question_3?.classList.add('active');
+    }
+    question4(){
+      let question_3=document.querySelector('.question3');
+      let question_4=document.querySelector('.question4');
+      question_3?.classList.remove('active');
+      question_4?.classList.add('active');
+    }
+    question5(){
+      let question_5=document.querySelector('.question5');
+      let question_4=document.querySelector('.question4');
+      question_4?.classList.remove('active');
+      question_5?.classList.add('active');
+    }
+    question6(){
+      let question_5=document.querySelector('.question5');
+      let question_6=document.querySelector('.question6');
+      question_5?.classList.remove('active');
+      question_6?.classList.add('active');
+    }
+    question7(){
+      let question_6=document.querySelector('.question6');
+      let question_7=document.querySelector('.question7');
+      question_6?.classList.remove('active');
+      question_7?.classList.add('active');
+    }
+    question8(){
+      let question_7=document.querySelector('.question7');
+      let question_8=document.querySelector('.question8');
+      question_7?.classList.remove('active');
+      question_8?.classList.add('active');
+    }
+    question9(){
+      let question_9=document.querySelector('.question9');
+      let question_8=document.querySelector('.question8');
+      question_8?.classList.remove('active');
+      question_9?.classList.add('active');
+    }
+    question10(){
+      let question_10=document.querySelector('.question10');
+      let question_9=document.querySelector('.question9');
+      question_9?.classList.remove('active');
+      question_10?.classList.add('active');
     }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+
 
 
 
@@ -18,7 +21,8 @@ import {MatIconModule} from '@angular/material/icon';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
